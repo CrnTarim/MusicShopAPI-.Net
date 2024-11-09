760d2cfadc37da00fbd89efb2f078d37ecbe8914
CrnTarim <crntrim@gmail.com>
2024-11-09 17:49:34 +0300

    Messagings user by Using UserId

--- MusicShop.Infrastructure.Concrete/HubConnection/ChatHub.cs
+++ MusicShop.Infrastructure.Concrete/HubConnection/ChatHub.cs
@@ -10,14 +10,37 @@ using System.Threading.Tasks;
 namespace MusicShop.Infrastructure.Concrete.HubConnection
 
 {
-    public class ChatHub : Hub
+   public class ChatHub : Hub
+{
+   
+    private static ConcurrentDictionary<string, string> userConnectionMap = new ConcurrentDictionary<string, string>();
+
+    public async Task RegisterUser(string userId)
     {
-            
-        public async Task SendMessageToUser(string targetConnectionId, string message)
+      
+        userConnectionMap[userId] = Context.ConnectionId;
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+       
+        var userId = userConnectionMap.FirstOrDefault(x => x.Value == Context.ConnectionId).Key;
+        if (userId != null)
+        {
+            userConnectionMap.TryRemove(userId, out _);
+        }
+
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task SendMessageToUser(string targetUserId, string message)
+    {
+        if (userConnectionMap.TryGetValue(targetUserId, out var targetConnectionId))
         {
             await Clients.Client(targetConnectionId).SendAsync("ReceiveMessage", message);
         }
     }
+  }
 
 }
 
